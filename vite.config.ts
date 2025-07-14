6b861be65d48c26ef9b20b89f87ec91fcee365ef
leozam02 <ltzambra@espol.edu.ec>
2025-07-14 14:14:35 -0500

    Ccambios

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,49 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+import { VitePWA } from 'vite-plugin-pwa'
+
 export default defineConfig({
-  base: "/dashboard",
-  plugins: [react()],
+   base: "/dashboard/",
+   plugins: [
+      react(),
+      VitePWA({
+         registerType: 'autoUpdate',
+         devOptions: {
+            enabled: true
+         },
+         includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+          manifest: {
+              id: '/dashboard/',
+              name: 'Dashboard del Clima - Proyecto 04',
+              short_name: 'Dashboard del Clima',
+              description: 'Proyecto 04 - dashboard del clima desarrollado con React y MUI',
+              theme_color: '#D3D1D1',
+              icons: [
+                {
+                    src: 'pwa-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                },
+                {
+                    src: 'pwa-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                },
+                {
+                    src: "pwa-maskable-192x192.png",
+                    sizes: "192x192",
+                    type: "image/png",
+                    purpose: "maskable"
+                },
+                {
+                    src: "pwa-maskable-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png",
+                    purpose: "maskable"
+                },
+              ]
+          }
+      })
+   ]
 })
